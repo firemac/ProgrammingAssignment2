f49a539b500ce99f46152ed0809d4a4eb41fe74e
Mario <firemac79@gmail.com>
2014-10-20 13:05:03 -0500

    Scoping and Caching
    Lexical Scoping and Cache functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Lexical scoping and cache functions
+## solve() finds inverse of a matrix
 
-## Write a short comment describing this function
+
+## store a function 'test' using makeCacheMatric() function.
 
 makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  set<-function(y){
+  x<<-y
+  m<<-NULL
+}
+get<-function() x
+setmatrix<-function(solve) m<<- solve
+getmatrix<-function() m
+list(set=set, get=get,
+   setmatrix=setmatrix,
+   getmatrix=getmatrix)
 
 }
 
 
-## Write a short comment describing this function
+## caching a free floating variable
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m<-x$getmatrix()
+    if(!is.null(m)){
+      message("getting cached data")
+      return(m)
+    }
+    matrix<-x$get()
+    m<-solve(matrix, ...)
+    x$setmatrix(m)
+    m
+
 }
+
+test<-makeCacheMatrix()
+test$set(matrix(1:4,2,2))
+
+cacheSolve(test)
